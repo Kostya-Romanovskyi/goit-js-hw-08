a573b3805d7559ea5c18083b4996c0b88814f6eb
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2022-12-16 00:11:43 +0200

    added second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Vimeo from '../../node_modules/@vimeo/player/src/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    // data is an object containing properties specific to that event
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(Math.round(data.seconds)));
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
