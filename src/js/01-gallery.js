15d3962c973d4f8e0c265dac19a3223326c1f7ee
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2022-12-15 23:25:01 +0200

    add npm to gallary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Add imports above this line\
 
+//added simplelightBox library
+import SimpleLightbox from 'simplelightbox';
+
+//added styles for library`s markup
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+//backend
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+const galary = document.querySelector('.gallery');
+
+const itemsMarkup = onCreateGalaryElements();
+
+// створення розмітки елементів галереї
+function onCreateGalaryElements() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+             <a class="gallery__item" href="${original}">
+                 <img class="gallery__image" 
+                 loading="lazy"
+                   src="${preview}" 
+                   alt="${description}" />
+            </a>
+                </div>`;
+    })
+    .join('');
+}
+
+galary.innerHTML = itemsMarkup;
+
+// Підключення та налаштування бібліотеки
+var lightbox = new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+  widthRatio: 0.9,
+  heightRatio: 0.9,
+});
 console.log(galleryItems);
